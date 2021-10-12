36b959140d7d7db965abfc13785524730f4bf4a5
Jakub Góralczyk <s23441@pjwstk.edu.pl>
2021-10-12 16:48:23 +0200

    koniec

--- lab01/demo/src/main/java/org/example/Application.java
+++ lab01/demo/src/main/java/org/example/Application.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import javax.security.auth.Subject;
+import java.lang.reflect.Method;
+import java.util.List;
+
 public class Application {
 
+
     public static void main(String[] args){
+        Class myClass = new myClass().getClass();
+        ObjectPropertyProvider provider = new ObjectPropertyProvider();
+        List<Method> methods = provider.getPublicGetters(myClass);
+
+        for (Method method:methods) {
+            System.out.println(method.getName());
+        }
+
+
+
+
 
     }
 }
--- lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
@@ -3,30 +3,66 @@ package org.example;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ObjectPropertyProvider {
 
     public List<Method> getPublicGetters(Class<?> clazz){
-        Method m = Arrays.stream(clazz.getDeclaredMethods()).findFirst().or(null).get();
 
+        List<Method> methods = Arrays.asList(clazz.getMethods());
+        List<Method> getterMethods = new ArrayList<>();
 
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
-
+        for (Method method:methods) {
+            if (isGetter(method))
+            {
+                getterMethods.add(method);
+            }
+        }
+        return getterMethods;
+        
     }
 
 
     public List<Method> getPublicSetters(Class<?> clazz){
-
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+        List<Method> methods = Arrays.asList(clazz.getMethods());
+        List<Method> setterMethods = new ArrayList<>();
+        for (Method method:methods) {
+            if (isSetter(method))
+            {
+                setterMethods.add(method);
+            }
+        }
+        return setterMethods;
     }
 
 
     public List<Field> getFieldsForPublicProperties(Class<?> clazz){
+        List<Method> methods = Arrays.asList(clazz.getMethods());
+        List<Field> fields = Arrays.asList(clazz.getFields());
+        for (Method method:methods) {
+            if (isSetter(method) || isSetter(method))
+            {
+                //fields.add(method.);
+            }
+        }
 
-        return Arrays.stream(clazz.getDeclaredFields()).toList();
+        return fields;
 
+   }
+
+    public static boolean isGetter(Method method){
+        if(!method.getName().startsWith("get"))      return false;
+        if(method.getParameterTypes().length != 0)   return false;
+        if(method.getReturnType().equals(void.class)) return false;
+        return true;
+    }
+
+    public static boolean isSetter(Method method){
+        if(!method.getName().startsWith("set")) return false;
+        if(method.getParameterTypes().length != 1) return false;
+        return true;
     }
 
 
