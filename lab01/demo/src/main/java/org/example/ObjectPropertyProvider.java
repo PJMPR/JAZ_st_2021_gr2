1df51ac11eb5a5a154737555200ba0fb4b63b8f0
Jakub Gola <s23449@pjwstk.edu.pl>
2021-10-12 16:50:36 +0200

    Zadanie 1

--- lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
@@ -1,33 +1,45 @@
 package org.example;
 
-
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ObjectPropertyProvider {
 
-    public List<Method> getPublicGetters(Class<?> clazz){
-        Method m = Arrays.stream(clazz.getDeclaredMethods()).findFirst().or(null).get();
-
-
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+    public static boolean isGetter(Method m){
+        if(Modifier.isPublic((m.getModifiers())) && m.getParameterTypes().length == 0)
+            if(m.getName().startsWith("get") && !m.getReturnType().equals(void.class))
+                return true;
+            if(m.getName().startsWith("is") && !m.getReturnType().equals(boolean.class))
+                return true;
+        return false;
 
     }
 
+    public List<Method> getPublicGetters(Class<?> clazz) {
 
-    public List<Method> getPublicSetters(Class<?> clazz){
+        Method[] methods = clazz.getDeclaredMethods();
+        ArrayList<Method> gettersList = new ArrayList<>();
 
+        for (Method method : methods) {
+            if(isGetter(method))
+                gettersList.add(method);
+        }
+        return gettersList;
+    }
+
+
+    public List<Method> getPublicSetters(Class<?> clazz) {
         return Arrays.stream(clazz.getDeclaredMethods()).toList();
     }
 
 
-    public List<Field> getFieldsForPublicProperties(Class<?> clazz){
-
+    public List<Field> getFieldsForPublicProperties(Class<?> clazz) {
         return Arrays.stream(clazz.getDeclaredFields()).toList();
-
     }
-
-
 }
+
