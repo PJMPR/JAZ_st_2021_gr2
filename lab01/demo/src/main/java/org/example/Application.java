939c3dda1e7e394ff7efa2ba180c7fa0b01706c1
Kacper Kamm <s23045@pjwstk.edu.pl>
2021-10-12 16:47:35 +0200

    prawie zrobione

--- lab01/demo/src/main/java/org/example/Application.java
+++ lab01/demo/src/main/java/org/example/Application.java
@@ -1,8 +1,31 @@
 package org.example;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 public class Application {
 
-    public static void main(String[] args){
-
+    public static boolean isGetter(Method method){
+        if(!Modifier.toString(method.getModifiers()).startsWith("public")) return false;
+        if(!method.getName().startsWith("get"))      return false;
+        if(!method.getName().startsWith("is"))      return false;
+        if(method.getParameterTypes().length != 0)   return false;
+        return true;
     }
+
+    public static boolean isSetter(Method method){
+        if(!Modifier.toString(method.getModifiers()).startsWith("public")) return false;
+        if(!method.getName().startsWith("set")) return false;
+        if(method.getParameterTypes().length != 1) return false;
+        if(!void.class.equals(method.getReturnType())) return false;
+        return true;
+    }
+    public static boolean haveField(Method method){
+        if(!method.getName().startsWith("get")) return false;
+        if(!method.getName().startsWith("is")) return false;
+        if(!method.getName().startsWith("set")) return false;
+        if(!method.getName().contains("field")) return false;
+        return true;
+    }
+
 }
