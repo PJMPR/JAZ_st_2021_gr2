22cdf05114dff0b98446b4501e809ded7546b1ff
Szymon Kwidzinski <s23662@pjwstk.edu.pl>
2021-10-12 16:39:00 +0200

    a

--- lab01/demo/src/main/java/org/example/Application.java
+++ lab01/demo/src/main/java/org/example/Application.java
@@ -1,7 +1,27 @@
 package org.example;
 
-public class Application {
+import org.example.ObjectPropertyProvider;
+import org.testng.annotations.Test;
 
+import javax.security.auth.Subject;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public void abstract class Application {
+    public String name;
+
+    private Object provider;
+    List<String> getters = provider.getPublicGetters(Subject.class)
+            .stream().map(x->x.getName()).toList();
+    
+    public void getName(String name) {
+        this.name = name;
+    }
+    public abstract <name> String getName(this.name);
+    public abstract String getStatus();
+    public abstract String getNumber(int ,'5');
     public static void main(String[] args){
 
     }
