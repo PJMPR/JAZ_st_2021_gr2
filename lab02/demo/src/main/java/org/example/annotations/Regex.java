91debfa1a78dd9ef364aa02382d66756752abaf1
Leshens <kacper.kamm@gmail.com>
2021-10-27 21:50:47 +0200

    fix Regex default

--- lab02/demo/src/main/java/org/example/annotations/Regex.java
+++ lab02/demo/src/main/java/org/example/annotations/Regex.java
@@ -1,5 +1,4 @@
 package org.example.annotations;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,7 +7,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Regex {
-    String pattern();
+    String pattern() default "^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$";
 
     String message() default "regex error";
 }
