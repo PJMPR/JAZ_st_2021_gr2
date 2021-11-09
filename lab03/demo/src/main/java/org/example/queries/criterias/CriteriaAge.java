ac20fb83c844149e5c6de0cd8f576a57527aa66d
PJMPR <adam.urbanow@gmail.com>
2021-11-09 15:37:54 +0100

    Update CriteriaAge.java

--- lab03/demo/src/main/java/org/example/queries/criterias/CriteriaAge.java
+++ lab03/demo/src/main/java/org/example/queries/criterias/CriteriaAge.java
@@ -6,30 +6,52 @@ import org.example.queries.search.SearchParameters;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class CriteriaAge implements Criteria {
+public abstract class CriteriaBase implements Criteria{
 
-    @Override
+   @Override
     public List<Person> meetCriteria(List<Person> persons, SearchParameters searchParameters) {
-
-        if(searchParameters.getAgeTo() != 0) {
+        this.parameters = searchParameters;
+        if(checkParameters()) {
             persons = persons.stream()
-                    .filter(person -> person.getAge() <= searchParameters.getAgeTo())
+                    .filter(person -> checkCriteria(person))
                     .collect(Collectors.toList());
         }
+    }
+        protected SearchParameters parameters;
+        protected abstract boolean checkParameters();
+        protected abstract boolean checkCriteria(Person p);
+    
+        
+}
 
-        if(searchParameters.getAgeFrom() != 0) {
-            persons = persons.stream()
-                    .filter(person -> person.getAge() >= searchParameters.getAgeFrom())
-                    .collect(Collectors.toList());
-        }
-
-        return persons;
-
+public class SimpleCriteria extends CriteriaBase{
+    Predicate<SearchParameters> checkParams;
+    Predicate<Person> checkPerson;
+    
+    public SimpleCriteria(Predicate<SearchParameters> checkParams, Predicate<Person> checkPerson){
+        this.checkPerson=checkPerson;
+        this.checkParams = checkParams;
+    }
+    
+    protected boolean checkParameters(){
+        return this.checkParams.check(parameters);
     }
 
-    @Override
-    public boolean isSet(SearchParameters searchParameters) {
-        return true;
+    
+    protected boolean checkCriteria(Person p){
+        return this.checkPerson.check(p);
+    }
+    
+}
+
+public class CriteriaAgeFrom extends CriteriaBase {
+
+    protected boolean checkParameters(){
+        return parameters.getAgeFrom()>0;
     }
 
+    
+    protected boolean checkCriteria(Person p){
+        return p.getAge()>parameters.getAgeFrom;
+    }
 }
