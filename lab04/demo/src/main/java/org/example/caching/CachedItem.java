7f4eb6d6347a851690aa87b41629cd1e9cc56d61
Ian Canals <canals.ian@gmail.com>
2021-11-11 20:38:18 +0100

    all tests pass

---
+++ lab04/demo/src/main/java/org/example/caching/CachedItem.java
@@ -0,0 +1,25 @@
+package org.example.caching;
+
+
+public class CachedItem<T> {
+
+    private T item;
+    private String key;
+
+    public CachedItem(String key, T item) {
+        this.key = key;
+        this.item = item;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public T getItem(){
+        return item;
+    }
+
+    public String getKey() {
+        return key;
+    }
+}
