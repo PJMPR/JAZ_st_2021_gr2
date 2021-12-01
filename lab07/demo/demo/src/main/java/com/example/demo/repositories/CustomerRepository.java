69d0b911f0855d49932a9e5eb57141586d46f9b6
adam urbanowicz <adam.urbanow@gmail.com>
2021-12-01 16:59:52 +0100

    lab07 fix model

--- lab07/demo/demo/src/main/java/com/example/demo/repositories/CustomerRepository.java
+++ lab07/demo/demo/src/main/java/com/example/demo/repositories/CustomerRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repositories;
 
-import com.example.demo.data.Customer;
+import com.example.demo.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
