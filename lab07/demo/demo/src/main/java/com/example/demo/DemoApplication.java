2b579f53bd7fec6c6d222179f82e27a51fc20c23
RandomE-del <kuba300901@gmail.com>
2021-12-01 23:58:00 +0100

    day 1

--- lab07/demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ lab07/demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,13 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.persistence.EntityManager;
-
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
