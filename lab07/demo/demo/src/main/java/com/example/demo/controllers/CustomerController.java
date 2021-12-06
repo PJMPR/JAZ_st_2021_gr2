996b2fc7e9b4b10028eae920774b1fa3dc74f78f
Ian Canals <canals.ian@gmail.com>
2021-12-07 00:03:42 +0100

    lab07

--- lab07/demo/demo/src/main/java/com/example/demo/controllers/CustomerController.java
+++ lab07/demo/demo/src/main/java/com/example/demo/controllers/CustomerController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+<<<<<<< Updated upstream
 import com.example.demo.repositories.CustomerRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,5 +31,32 @@ public class CustomerController {
                 .stream()
                 .map(x->x.getLastUpdate())
                 .collect(Collectors.toList()));
+=======
+import com.example.demo.DTOModel.CustomerDTO;
+import com.example.demo.repositories.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class CustomerController {
+    CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("customers/ranking/bySpentMoney")
+    public ResponseEntity<List<CustomerDTO>> giveTopTenCustomersBySpentMoney(){
+        return ResponseEntity.ok(customerService.setCustomers().stream()
+                .sorted(Comparator.comparing(CustomerDTO::getSpent).reversed())
+                .limit(10).collect(Collectors.toList()));
+>>>>>>> Stashed changes
     }
 }
--- lab07/demo/demo/src/main/java/com/example/demo/repositories/CustomerRepository.java
+++ lab07/demo/demo/src/main/java/com/example/demo/repositories/CustomerRepository.java
@@ -1,6 +1,10 @@
 package com.example.demo.repositories;
 
+<<<<<<< Updated upstream
 import com.example.demo.model.Customer;
+=======
+import com.example.demo.DBModel.Customer;
+>>>>>>> Stashed changes
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
